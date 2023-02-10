d003064779fae795d6ae3be749f36cb52438b3c2
Rex-1031 <steventclair@gmail.com>
2023-02-10 10:36:53 -0500

    working on bugs

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 
 const allowedOrigins = [
-    'https://technotes-6e0c.onrender.com/'
+    'https://technotes-6e0c.onrender.com/',
+    'https://technotes.onrender.com/'
+
 ]
 
 module.exports = allowedOrigins
